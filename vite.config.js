b158f85deaf91d909e0cddf8c6baf9348095353a
miachen0413 <spispil332@gmail.com>
2024-12-16 13:47:58 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       '/api': {
         // target: 'https://my-vercel-api-one.vercel.app/api',
         // target: 'http://localhost:3000/api',
-        target: 'https://my-api-neon.vercel.app//api',
+        target: 'https://my-api-neon.vercel.app/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
